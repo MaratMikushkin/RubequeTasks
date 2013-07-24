d5465b2a72c7bfd42771c3dacbd5f323eba7dab8
marat <marat.miki@gmail.com>
2013-07-24 16:17:43 +0400

    adding key_for_min_value.rb

---
+++ tests/test_key_for_min_value.rb
@@ -0,0 +1,16 @@
+require '../lib/baby_got_stack.rb'
+require 'minitest/autorun'
+
+class TestKeyForMinValue < MiniTest::Unit::TestCase
+  def setup
+    @register = KeyForMinValue.new
+  end
+
+  def test_
+    assert_equal "j", \
+        @register.key_for_min_value({"k" => 2, "h" => 3, "j" => 1})
+    assert_equal "z", \
+        @register.key_for_min_value({"o" => 0, "z" => -2, "j" => 1})
+    assert_equal nil, @register.key_for_min_value({})
+  end
+end
